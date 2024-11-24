11bf421cd113cd8c7cd92b546459cbcabe848e47
Aryan Jain <2006aryanj@gmail.com>
2024-11-24 03:59:04 -0500

    Fixed nav bar css and added a logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 function NavBar() {
   return (
     <nav className = 'nav'>
-      <Link to="/" className = "site-title"> AppDev Educational Portal </Link>
+      <Link to="/" className = "logo"><img src="assets/logo.png" alt="logo" /></Link>
       <ul>
         <li> <Link to="/calendar"> Calendar </Link> </li>
         <li> <Link to="/lectures"> Lectures </Link> </li>
