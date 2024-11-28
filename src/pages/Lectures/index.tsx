d8d845147dbba4dfba747d5096c2e6cdf8de2ade
Aryan Jain <2006aryanj@gmail.com>
2024-11-28 14:25:08 -0500

    started slides css

--- src/pages/Lectures/index.tsx
+++ src/pages/Lectures/index.tsx
@@ -1,25 +1,29 @@
 // @ts-ignore
 import React from 'react';
+// Swiper Imports
 import { Navigation, Pagination, A11y } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.css';
+// CSS Imports
+import classes from './Lectures.module.css';
 
 function Lectures() {
   return (
-    <div>
+    <div className={classes.container}>
       <Swiper
+        className={classes.swiperContainer}
         modules={[Navigation, Pagination, A11y]}
-        spaceBetween={50}
-        slidesPerView={3}
+        spaceBetween={20}
+        slidesPerView={2.5}
         navigation
         pagination={{ clickable: true }}
         onSwiper={(swiper) => console.log(swiper)}
         onSlideChange={() => console.log('slide change')}
       >
-        <SwiperSlide className = "slide">Slide 1</SwiperSlide>
-        <SwiperSlide className = "slide">Slide 2</SwiperSlide>
-        <SwiperSlide className = "slide">Slide 3</SwiperSlide>
-        <SwiperSlide className = "slide">Slide 4</SwiperSlide>
+        <SwiperSlide className={classes.slide}>Slide 1</SwiperSlide>
+        <SwiperSlide className={classes.slide}>Slide 2</SwiperSlide>
+        <SwiperSlide className={classes.slide}>Slide 3</SwiperSlide>
+        <SwiperSlide className={classes.slide}>Slide 4</SwiperSlide>
       
       </Swiper>
 
