3e9a3465e8190c46d70d8bb755502b29b9f87c4c
Aryan Jain <2006aryanj@gmail.com>
2024-11-27 19:04:30 -0500

    added data

--- src/data/profiles.ts
+++ src/data/profiles.ts
@@ -6,7 +6,15 @@ export const profilesData = [
         websiteURL: "https://www.gavinkhung.me/", 
         resumeURL: "https://www.gavinkhung.me/resume.pdf"
     },
-    {id: 2, name: "Gavin Hung", status: "Mentor", 
+    {id: 2, name: "Nitish", status: "Mentor", 
+        imgURL: "https://media.licdn.com/dms/image/v2/D5603AQGurWafPZmY3A/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1712186812396?e=1737590400&v=beta&t=yE9tgwJHeozPocrvCKy3aiH1kaA5uHKRne94nnLeSW4", 
+        linkedinURL: "https://www.linkedin.com/in/gavinkhung/", 
+        gitHubURL: "https://github.com/gavinkhung", 
+        websiteURL: "https://www.gavinkhung.me/", 
+        resumeURL: "https://www.gavinkhung.me/resume.pdf"
+    },
+
+    {id: 3, name: "Dhruv", status: "Student", 
         imgURL: "https://media.licdn.com/dms/image/v2/D5603AQGurWafPZmY3A/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1712186812396?e=1737590400&v=beta&t=yE9tgwJHeozPocrvCKy3aiH1kaA5uHKRne94nnLeSW4", 
         linkedinURL: "https://www.linkedin.com/in/gavinkhung/", 
         gitHubURL: "https://github.com/gavinkhung", 
