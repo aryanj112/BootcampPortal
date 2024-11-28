43bed9d9ecfd481622efcb137c93b2f69df39a90
Rushil <rushiljuneja@gmail.com>
2024-11-28 16:29:10 -0700

    Home Feature Done

---
+++ src/data/HomeworkData.ts
@@ -0,0 +1,41 @@
+export const homeworkData = [
+    { id: 1, name: "Memory Card Mathching Game", description: "Our homework has two parts this week. The first part is reading " + 
+        "React documentation before class this week and completing some React challenges. This documentation is how I personally " + 
+        "learned React and I really recommend reading the entire “Describing the UI” section if you can. This will put you ahead " + 
+        "and give you a firm understanding of React. Your homework assignment is to read " + 
+        "through https://react.dev/learn/your-first-component https://react.dev/learn/passing-props-to-a-component " + 
+        "and https://react.dev/learn/conditional-rendering and then complete the challenges for ‘Your first component’ and ‘Conditional " + 
+        "Rendering’ and then take screenshots upon completion. I know the solutions are available on the website, but please do them on " + 
+        "your own (they are for your own good).\n\n" +  "Our second set of homework this week is another JavaScript game. I have the code " + 
+        "for it in the GitHub Repository button. It should look like the photo when you’re all done. Please do not clone the repository, " + 
+        "just download the files or copy and paste them into your editor and then make your own GitHub repository and you will submit the link.",
+        githubLink: "", submissionPortal: "https://forms.gle/SikXYsLiDJnQCgr28", dueDate: "10/34/24", images: ["src/assets/hw1_img1.png"] },
+    
+    { id: 2, name: "Flash Card App", description: "For our homework, we are creating a flashcard app! When you click the card, " + 
+        "it should show the definition. You can also go to the next card or previous card. If you keep pressing next, it should " + 
+        "loop back to the first card and vice-versa. Also, you need to create the ability to add more cards if needed. I posted " + 
+        "some photos of how the app should look after you’re done. Please stop by office hours if you have any questions. To get " + 
+        "the files click the GitHub Repository button, please do not clone the repository. Instead, copy and paste the code into " + 
+        "your own code editor or download a zip and get the files that way. When you’re done, make sure to create your own repository " + 
+        "for the files and submit it on the Submission Portal Google form", githubLink: "https://github.com/KiberVG/bootcamp2024-flashcardhw",
+        submissionPortal: "https://forms.gle/BiJU8QxU1NA43k9d6", dueDate: "10/13/24", images: ["src/assets/hw2_img1.png", "src/assets/hw2_img2.png"] },
+    
+    { id: 3, name: "Twitter Clone", description: "Hey everyone! I hope you all enjoyed our React lecture. This week you will be creating a " + 
+        "Twitter Clone! \n\nIn this project you will be practicing creating components, passing down props, conditional rendering, and using " + 
+        "state! You do not need to add the functionality to create new tweets or comments, you simply need to display the tweets on the " + 
+        "screen and have the ability to like and unlike the tweets. \n\nAfter clicking on the GitHub link, do not clone the project. Instead, " + 
+        "press the green ‘code’ button and press ‘Download ZIP’. Then you can unzip it and create a new repository from there. \n\nAlso, we " + 
+        "have a couple more reading assignments for this week as well. \n\nhttps://react.dev/learn/rendering-lists (no challenges) " + 
+        "\nhttps://react.dev/learn/state-a-components-memory (complete challenges) \nhttps://react.dev/learn/render-and-commit (no challenges) " + 
+        "\nhttps://react.dev/learn/updating-objects-in-state (no challenges) \n\nIt’s important that the reading gets done before Tuesday’s " + 
+        "lecture even if you can’t submit the form yet. Good luck everyone have a great rest of your week and come to office hours!", 
+        githubLink: "https://github.com/KiberVG/bootcampfall2024-twitterclone", submissionPortal: "https://forms.gle/8SXRVwGs4q2MQHJD6", 
+        dueDate: "11/06/24", images: ["src/assets/hw3_img1.png"] },
+    
+    { id: 4, name: "Portfolio Website", description: "For this week’s homework get started on creating your personal website portfolio! " + 
+        "\n\nSimilar to class today, create your own website portfolio using HTML and CSS \n\nYour website must have your headshot photo, " + 
+        "h1 and h2 tags, p tags, a tags, and different sections about yourself including an about section and an experience section. " + 
+        "\n\nStyle your website using CSS. Your website should overall look good and use the things we learned in class.  \n\nKeep studying " + 
+        "HTML and CSS (practice makes perfect)", githubLink: "", submissionPortal: "https://forms.gle/FtfsBKiXwcuTmXLw7", dueDate: "10/09/24", 
+        images: [] }
+];
--- src/pages/Homework.tsx
+++ src/pages/Homework.tsx
@@ -1,9 +1,91 @@
-import React from 'react'
+import React from 'react';
+import { homeworkData } from '../data/HomeworkData.ts';
 
 function Homework() {
   return (
-    <div>Homework</div>
-  )
+    <>
+      <div className="homework-heading">
+        <h1>Homework Assignments</h1>
+      </div>
+      <HomeworkList />
+    </>
+  );
 }
 
-export default Homework
+const transformDescription = (description: string) => {
+  const formattedDescription = description.replace(/\n/g, '<br />');
+
+  const urlRegex = /(https?:\/\/[^\s]+)/g;  
+  return formattedDescription.split(urlRegex).map((part, index) => 
+    urlRegex.test(part) ? (
+      <a key={index} href={part} target="_blank" rel="noopener noreferrer" className="homework-link">
+        {part}
+      </a>
+    ) : (
+      <span key={index} dangerouslySetInnerHTML={{ __html: part }} />
+    )
+  );
+};
+
+const HomeworkCard: React.FC<{ homework: typeof homeworkData[0] }> = ({ homework }) => (
+  <div className="homework-card">
+    <div className="homework-content">
+      <div className="homework-text">
+        <h2>{homework.name}</h2>
+        <p>Due Date: {homework.dueDate}<br />(11:59 PM)</p>
+        {homework.githubLink !== "" && (
+          <a
+            href={homework.githubLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="homework-link"
+          >
+            GitHub Repository<br /><br />
+          </a>
+        )}
+        <a
+          href={homework.submissionPortal}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="homework-link"
+        >
+          Submission Portal
+        </a>
+      </div>
+      <div className="homework-description">
+        <p>
+          <b>Description:</b>
+          <br /> <br /> {transformDescription(homework.description)}
+        </p>
+      </div>
+      <div className="homework-images">
+        {homework.images.map((img, index) => (
+          <a
+            key={index}
+            href={img} 
+            target="_blank"
+            rel="noopener noreferrer"
+            className="homework-image-button"
+          >
+            <img
+              src={img}
+              alt={`Homework ${homework.id} Image ${index + 1}`}
+            />
+          </a>
+        ))}
+      </div>
+    </div>
+  </div>
+);
+
+const HomeworkList: React.FC = () => {
+  return (
+    <div className="homework-list-container">
+      {homeworkData.map(hw => (
+        <HomeworkCard key={hw.id} homework={hw} />
+      ))}
+    </div>
+  );
+};
+
+export default Homework;
