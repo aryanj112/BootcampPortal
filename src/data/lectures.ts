9d680ea2b29529c5ff3216b7830977ded8ec27a2
Aryan Jain <2006aryanj@gmail.com>
2024-12-01 17:52:03 -0500

    minor changes

---
+++ src/data/lectures.ts
@@ -0,0 +1,44 @@
+export const lecturesData = [
+    {
+        id: 1, 
+        title: "",
+        date: "",
+        lecturer: "",
+        description: "",
+        slideURL: "https://docs.google.com/presentation/d/1wO047LhrT73QIcC5WzFhzGOtYhxap3aPq-nYbjmOSKk/edit#slide=id.g1f1cb8c2007_0_209",
+        zoomURL: "https://umd.zoom.us/rec/component-page?action=viewdetailpage&sharelevel=meeting&useWhichPasswd=meeting&clusterId=aw1&componentName=need-password&meetingId=5z0PYgVgimujBts5SpDPktPwHpVDNRoSD5-3KBdIfkzeGwDa46Q76Dl2C6euwp6N.WGIa3JSrqpYzcqlE&originRequestUrl=https%3A%2F%2Fumd.zoom.us%2Frec%2Fshare%2F9hW7NpHQ4nNouT6DAj6vWsIf9ahn6oHv4IroPvjLq3R98wfPNyVMzgJqWQ4nEIG4.HTyCMn3NAWB-ETkj%3FstartTime%3D1727824735000", 
+        zoomPassword: "u98!&ye*"
+    },
+    {
+        id: 2, 
+        title: "",
+        date: "",
+        lecturer: "",
+        description: "",
+        slideURL: "",
+        zoomURL: "",
+        zoomPassword: ""
+    },
+    {
+        id: , 
+        title: "",
+        date: "",
+        lecturer: "",
+        description: "",
+        slideURL: "",
+        zoomURL: "",
+        zoomPassword: ""
+    },
+    {
+        id: , 
+        title: "",
+        date: "",
+        lecturer: "",
+        description: "",
+        slideURL: "",
+        zoomURL: "",
+        zoomPassword: ""
+    },
+
+
+]
