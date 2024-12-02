d9c5cda82289248ca54adad794fba9ddc559770e
Aryan Jain <2006aryanj@gmail.com>
2024-12-02 16:30:07 -0500

    added basic log in system

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,13 +1,15 @@
-// @ts-ignore
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from "../../assets/logo.png";
 import './NavBar.css';
-
-
-// className={`${classes.nav} ${classes.active}`} is how you would add multiple classes
+import './LoginModel.css';
 
 function NavBar() {
+
+  const [isModelOpen, setIsModelOpen] = useState(false);
+  const handleLoginClick = () => setIsModelOpen(true);
+  const handleCloseModel = () => setIsModelOpen(false);
+
   return (
     <nav className="nav">
       <Link to="/" className="logo">
@@ -19,9 +21,47 @@ function NavBar() {
         <li> <Link to="/attendence"> Attendence </Link> </li>
         <li> <Link to="/group"> Group </Link> </li>
       </ul>
-      <button className = "login-button"> Login </button>
+      <button onClick = {handleLoginClick} className = "login-button"> Login </button>
+      {isModelOpen && <LoginModel onClose = {handleCloseModel}/>}
     </nav>
   )
 }
+
+interface LoginModalProps {
+  onClose: () => void;
+}
+
+function LoginModel({ onClose }: LoginModalProps){
+  
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log({ name, password }); // Replace this with a save action (e.g., API call)
+    localStorage.setItem('user', JSON.stringify({ name, password }));
+    onClose();
+  };
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal-box">
+        <button onClick={onClose} className="close-button">X</button>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Name:
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+          </label>
+          <label>
+            Password:
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          </label>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
 
 export default NavBar;
