b38b17eaaaf3e2a12a4c9edc5dd66b561dd62ebd
Rushil <rushiljuneja@gmail.com>
2024-12-02 18:58:50 -0500

    copy pasted homework into lectures

--- src/pages/Lectures/index.tsx
+++ src/pages/Lectures/index.tsx
@@ -1,54 +1,17 @@
-// @ts-ignore
 import React from 'react';
-// Swiper Imports
-import { Navigation, A11y, EffectCoverflow } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.css';
-
-// CSS Imports
-import './Lectures.css';
+import { homeworkData } from '../../data/HomeworkData.ts';
+import '../Homework/homeworkStyling.css';
 
 function Lectures() {
-
   return (
-    <div className="footerAddition">
-      <div className='container'>
-        <h1>Lectures</h1>
-        <Swiper
-          className="swiper_container"
-          effect={'coverflow'}
-          grabCursor={true}
-          loop={true}
-          slidesPerView={'auto'}
-          coverflowEffect={{
-            rotate: 0,
-            stretch: 0,
-            depth: 100,
-            modifier: 2.5,
-          }}
-          navigation={{
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-          }}
-          modules={[EffectCoverflow, Navigation, A11y]}
-          onSlideChange={() => console.log('Slide changed')}
-        >
-          <SwiperSlide className='slide'>Slide 1</SwiperSlide>
-          <SwiperSlide className='slide'>Slide 2</SwiperSlide>
-          <SwiperSlide className='slide'>Slide 3</SwiperSlide>
-          <SwiperSlide className='slide'>Slide 4</SwiperSlide>
-          <SwiperSlide className='slide'>Slide 5</SwiperSlide>
-          <SwiperSlide className='slide'>Slide 6</SwiperSlide>
-          <SwiperSlide className='slide'>Slide 7</SwiperSlide>
-          <SwiperSlide className='slide'>Slide 8</SwiperSlide>
-
-        <div className="swiper-button-prev slider-arrow"/>
-        <div className="swiper-button-next slider-arrow"/>
-
-        </Swiper>
-      </div>
-      <br /><br /><br /><br /><br /><br /><br />
-      <div className="footer">
+    <>
+      <div className="footerAddition">
+        <div className="homework-heading">
+          <h1>HOMEWORK ASSIGNMENTS</h1>
+        </div>
+        <HomeworkList />
+        <br /><br />
+        <div className="footer">
           <div className="footer-section">
           <h2>CONTACT INFORMATION</h2>
           <h3>KIMBER: martinezkimber03@gmail.com</h3>
@@ -59,9 +22,86 @@ function Lectures() {
           <h3>TIME: Tues 7:00 - 8:00 PM</h3>
           <h3>LOCATION: CSI 1121</h3>
           </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
+
+const transformDescription = (description: string) => {
+  const formattedDescription = description.replace(/\n/g, '<br />');
+
+  const urlRegex = /(https?:\/\/[^\s]+)/g;  
+  return formattedDescription.split(urlRegex).map((part, index) => 
+    urlRegex.test(part) ? (
+      <a key={index} href={part} target="_blank" rel="noopener noreferrer" className="homework-link">
+        {part}
+      </a>
+    ) : (
+      <span key={index} dangerouslySetInnerHTML={{ __html: part }} />
+    )
+  );
+};
+
+const HomeworkCard: React.FC<{ homework: typeof homeworkData[0] }> = ({ homework }) => (
+  <div className="homework-card">
+    <div className="homework-content">
+      <div className="homework-text">
+        <h2>{homework.name}</h2>
+        <p>Due Date: {homework.dueDate}<br />(11:59 PM)</p>
+        {homework.githubLink !== "" && (
+          <a
+            href={homework.githubLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="homework-link"
+          >
+            GitHub Repository<br /><br />
+          </a>
+        )}
+        <a
+          href={homework.submissionPortal}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="homework-link"
+        >
+          Submission Portal
+        </a>
+      </div>
+      <div className="homework-description">
+        <p>
+          <b>Description:</b>
+          <br /> <br /> {transformDescription(homework.description)}
+        </p>
+      </div>
+      <div className="homework-images">
+        {homework.images.map((img, index) => (
+          <a
+            key={index}
+            href={img} 
+            target="_blank"
+            rel="noopener noreferrer"
+            className="homework-image-button"
+          >
+            <img
+              src={img}
+              alt={`Homework ${homework.id} Image ${index + 1}`}
+            />
+          </a>
+        ))}
+      </div>
+    </div>
+  </div>
+);
+
+const HomeworkList: React.FC = () => {
+  return (
+    <div className="homework-list-container">
+      {homeworkData.map(hw => (
+        <HomeworkCard key={hw.id} homework={hw} />
+      ))}
+    </div>
+  );
+};
 
 export default Lectures;
