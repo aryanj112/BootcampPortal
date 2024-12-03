7af1c168bb1d8fa902d5e5c221aaae2e293daeab
aditisethi15 <148648533+aditisethi15@users.noreply.github.com>
2024-12-03 17:38:30 -0500

    Updated profiles.ts

--- src/data/profiles.ts
+++ src/data/profiles.ts
@@ -1,5 +1,5 @@
 export const profilesData = [
-    {id: 1, name: "Gavin Hung", status: "Mentor", 
+    {id: 1, name: "Gavin", status: "Mentor", 
         imgURL: "https://media.licdn.com/dms/image/v2/D5603AQGurWafPZmY3A/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1712186812396?e=1737590400&v=beta&t=yE9tgwJHeozPocrvCKy3aiH1kaA5uHKRne94nnLeSW4", 
         linkedinURL: "https://www.linkedin.com/in/gavinkhung/", 
         gitHubURL: "https://github.com/gavinkhung", 
