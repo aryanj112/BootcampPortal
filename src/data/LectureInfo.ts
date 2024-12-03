02bc01fb7b60819039f58e210e4189da7bdc1fe1
Rushil <rushiljuneja@gmail.com>
2024-12-03 15:13:13 -0500

    check now

---
+++ src/data/LectureInfo.ts
@@ -0,0 +1,26 @@
+export const lectureData = [
+    {id: 1, date: "10/01/24", 
+        zoomLink: "https://umd.zoom.us/rec/share/9hW7NpHQ4nNouT6DAj6vWsIf9ahn6oHv4IroPvjLq3R98wfPNyVMzgJqWQ4nEIG4.HTyCMn3NAWB-ETkj?startTime=1727824735000", 
+        slidesLink: "https://docs.google.com/presentation/d/1wO047LhrT73QIcC5WzFhzGOtYhxap3aPq-nYbjmOSKk/edit#slide=id.g1f1cb8c2007_0_209",
+        password: "u98!&ye*",
+        GitHubLink: ""
+    },
+    {id: 2, date: "10/08/24", 
+        zoomLink: "https://umd.zoom.us/rec/share/SfirrOZAemFf_xSm3zhZq7j_3G9vTJfKWoXr4YIJD7V3pxjMpQe_AIjGT-Wk3Tdv.R_nKA8KwWqv8umwS?startTime=1728428448000", 
+        slidesLink: "https://docs.google.com/presentation/d/1RWvO8TQ_ueJyBdSHfZ6oNvq9E6J3rYovItbX_Q-r-44/edit",
+        password: "p6jk0se$",
+        GitHubLink: ""
+    },
+    {id: 3, date: "10/15/24", 
+        zoomLink: "https://umd.zoom.us/rec/share/Rl_BFUApl78S6omhJPRFLdM2bqNJOC5NWiJkWk9nDVe9G6wf54-EZCFnD8WbokIK.aHGXOgs6ZGmg7pad?startTime=1729033180000", 
+        slidesLink: "https://docs.google.com/presentation/d/14ooPTPyM4QZPWMBq2sg4NypMQZAHQ4rY5n6CUn6l7zI/edit?usp=sharing",
+        password: "zP8i1D=B",
+        GitHubLink: "https://github.com/KiberVG/bootcampoct15code/blob/main/notes-app-2/script.js"
+    },
+    {id: 4, date: "10/22/24", 
+        zoomLink: "https://umd.zoom.us/rec/share/feRHep0QqN7yi6SJz8PwaQ6LrFP8BRJ96LSAf2lEhIxLeojaea27pPct65YuCS1-.sj_MOivR-ZpXWfqg?startTime=1729638243000", 
+        slidesLink: "https://docs.google.com/presentation/d/1YzEswdGs5zqZMaK8zPCaJl8PiiFFOYDnz2QVHLDAxak/edit?usp=sharing",
+        password: "A$NCN^j9",
+        GitHubLink: "https://github.com/KiberVG/bootcampoct15code/blob/main/notes-app-2/script.js"
+    }
+];
