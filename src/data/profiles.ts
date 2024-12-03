275038e98259b9010cd2a7ec9bc85e9a1ff49321
Aryan Jain <2006aryanj@gmail.com>
2024-12-03 15:31:30 -0500

    added profile data

--- src/data/profiles.ts
+++ src/data/profiles.ts
@@ -7,20 +7,25 @@ export const profilesData = [
         resumeURL: "https://www.gavinkhung.me/resume.pdf"
     },
     {id: 2, name: "Nitish", status: "Mentor", 
-        imgURL: "https://media.licdn.com/dms/image/v2/D5603AQGurWafPZmY3A/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1712186812396?e=1737590400&v=beta&t=yE9tgwJHeozPocrvCKy3aiH1kaA5uHKRne94nnLeSW4", 
-        linkedinURL: "https://www.linkedin.com/in/gavinkhung/", 
-        gitHubURL: "https://github.com/gavinkhung", 
-        websiteURL: "https://www.gavinkhung.me/", 
-        resumeURL: "https://www.gavinkhung.me/resume.pdf"
+        imgURL: "https://media.licdn.com/dms/image/v2/D4D03AQEa5gW0m0rK2w/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1710454008903?e=1738800000&v=beta&t=k-noAhPxE5f--Tovvi0EsIp1GqeTuVJDh-O07GjnFms",
+        linkedinURL: "https://www.linkedin.com/in/nitish-vobilisetti-swe/",
+        gitHubURL: "https://github.com/nitvob"
     },
 
-    {id: 3, name: "Dhruv", status: "Student", 
-        imgURL: "https://media.licdn.com/dms/image/v2/D5603AQGurWafPZmY3A/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1712186812396?e=1737590400&v=beta&t=yE9tgwJHeozPocrvCKy3aiH1kaA5uHKRne94nnLeSW4", 
-        linkedinURL: "https://www.linkedin.com/in/gavinkhung/", 
-        gitHubURL: "https://github.com/gavinkhung", 
-        websiteURL: "https://www.gavinkhung.me/", 
-        resumeURL: "https://www.gavinkhung.me/resume.pdf"
+    {
+        id: 3,
+        name: "Aryan",
+        status: "Student",
+        imgURL: "https://media.licdn.com/dms/image/v2/D4E35AQH0AwVtQ2-fug/profile-framedphoto-shrink_400_400/profile-framedphoto-shrink_400_400/0/1721141409771?e=1733724000&v=beta&t=jmpNYpfjBzGmvT3Ys0Uh5GBXDizN4Ffgk06a8d97lAE",
+        linkedinURL: "https://www.linkedin.com/in/aryanjain06/",
+        gitHubURL: "https://github.com/aryanj112"
+    },
+    {
+      id: 4,
+      name: "Aditi",
+      status: "Student",
+      imgURL: "https://media.licdn.com/dms/image/v2/D4E03AQEMlHUEHVLA2A/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1726514279485?e=1738800000&v=beta&t=uor6AokOWHCl5eOJTFTgg2f6TtXq0hCzFYWbjsCupXM",
+      linkedinURL: "https://www.linkedin.com/in/-aditisethi/",
+      gitHubURL: "https://github.com/aditisethi15"
     }
-
-
 ];
